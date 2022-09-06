32108b6a6ea4b051b4a60d758b0b11a3a76280f2
ArmandoBraho <105724648+ArmandoBraho@users.noreply.github.com>
2022-09-06 15:21:15 +0200

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-let lap1 = 34
-let lap2 = 33
-let lap3 = 36
+let scoreHome = document.getElementById("score-home");
+let scoreGuest = document.getElementById("score-guest");
+let newGameBtn = document.getElementById("new-game-btn");
 
-// Create a function that logs out the sum of all the lap times
+let plus1BtnGuest = document.getElementById("plus-1-btn-guest");
+let plus2BtnGuest = document.getElementById("plus-2-btn-guest");
+let plus3BtnGuest = document.getElementById("plus-3-btn-guest");
+let plus1BtnHome = document.getElementById("plus-1-btn-home");
+let plus2BtnHome = document.getElementById("plus-2-btn-home");
+let plus3BtnHome = document.getElementById("plus-3-btn-home");
 
-function sumLapTimes(){
-    console.log(lap1+lap2+lap3);
+
+
+function addPoints( squad, Points){
+    let actualPointsHome = parseInt(scoreHome.textContent);
+    let actualPointsGuest = parseInt(scoreGuest.textContent);
+    
+    if( squad === "home" ){
+        scoreHome.textContent = actualPointsHome + Points;
+        return;
+    }
+        scoreGuest.textContent = actualPointsGuest + Points;
 }
 
-sumLapTimes();
+function resetPoints(){
+    scoreHome.textContent = 0;
+    scoreGuest.textContent = 0;
+}
